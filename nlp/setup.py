a701561974b40d2961ddd7a4ec3528c71127cf6f
Kerlin Michel <michelkerlin@gmail.com>
2017-04-14 23:12:37 -0400

    implemented flesch-kincaid grade level readability formula

--- nlp/setup.py
+++ nlp/setup.py
@@ -1,2 +1,4 @@
 import nltk
 nltk.download('vader_lexicon')
+nltk.download('cmudict')
+nltk.download('punkt')
--- nlp/snlp.py
+++ nlp/snlp.py
@@ -1,6 +1,26 @@
+from nltk.corpus import cmudict
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 sentiment_analyzer = SentimentIntensityAnalyzer()
+pron_dict = cmudict.dict()
+
+def num_syl(word):
+  return [len(list(y for y in x if y[-1].isdigit())) for x in pron_dict[word.lower()]][0]
+
+def num_sent(text):
+  return len(sent_tokenize(text))
 
 def sentiment(text):
   return sentiment_analyzer.polarity_scores(text)
+
+def flesch_kincaid_grade_level(text):
+  words = word_tokenize(text.lower())
+  num_words = float(len(words))
+  num_syll = 0
+  for word in words:
+    if word in pron_dict:
+      num_syll += num_syl(word)
+  num_sents = float(num_sent(text))
+  num_syll = float(num_syll)
+  return (0.39*(num_words/num_sents)) + (11.8*(num_syll/num_words)) - 15.59
