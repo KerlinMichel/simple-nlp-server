a01dd132ad3a486d502664f2c1bba92d944e7476
Kerlin Michel <michelkerlin@gmail.com>
2017-04-14 23:20:55 -0400

    added route for flesch-kindcaid grade level formula

--- nlp/app.py
+++ nlp/app.py
@@ -11,5 +11,10 @@ def sentiment_analyze():
   text = request.get_json().get('text')
   return json.dumps(sentiment(text))
 
+@app.route("/flesch_kindcaid_grade_level", methods=['POST'])
+def fk_grade_level():
+  text = request.get_json().get('text')
+  return json.dumps(flesch_kincaid_grade_level(text))
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
